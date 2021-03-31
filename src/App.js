72e130231b5660e2530d5ae5162f5a2c60052397
AlinaPisarenko <78824916+AlinaPisarenko@users.noreply.github.com>
2021-03-31 18:28:56 -0400

    added search engine, font family

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <WeatherApp />
+      <footer>
+        This project was coded by Alina Pisarenko and is{" "}
+        <a
+          href="https://github.com/AlinaPisarenko/weather-app-project-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+      </footer>
     </div>
   );
 }
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,11 +1,45 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./WeatherApp.css";
 
 export default function WeatherApp() {
   return (
     <div className="WeatherApp">
-      <h1>Hello</h1>
-      <button class="btn btn-primary">hello</button>
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Time</li>
+        <li>Description</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+            alt="description"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: </li>
+            <li>Humidity: </li>
+            <li>Wind: </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
