178ce236b46c05f152dda16f7c6c9ff912416312
AlinaPisarenko <78824916+AlinaPisarenko@users.noreply.github.com>
2021-03-31 16:29:04 -0400

    created react app

---
+++ src/WeatherApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function WeatherApp() {
+  return (
+    <div className="WeatherApp">
+      <h1>Hello</h1>
+      <button class="btn btn-primary">hello</button>
+    </div>
+  );
+}
