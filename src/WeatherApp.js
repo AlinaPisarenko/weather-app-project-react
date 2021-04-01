8d9e08eeb6f02a1ccb63608349aefd91cfac5ea5
AlinaPisarenko <78824916+AlinaPisarenko@users.noreply.github.com>
2021-03-31 20:14:59 -0400

    Added CSS

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -6,7 +6,7 @@ export default function WeatherApp() {
   return (
     <div className="WeatherApp">
       <form>
-        <div className="row">
+        <div className="row search-engine">
           <div className="col-9">
             <input
               type="search"
@@ -15,30 +15,42 @@ export default function WeatherApp() {
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary search"
+            />
+            <input
+              type="submit"
+              value="O"
+              className="btn btn-primary current"
+            />
           </div>
         </div>
       </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Time</li>
-        <li>Description</li>
-      </ul>
       <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="description"
-          />
-          6°C
-        </div>
-        <div className="col-6">
+        <div className="col-4 main-info">
+          <h3>New York</h3>
+          <div className="row main-icon">
+            <div className="col-6">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+                alt="description"
+              />
+            </div>
+            <div className="col-6">
+              <h1>10</h1>
+              <h5>Time</h5>
+            </div>
+          </div>
           <ul>
+            <li>Description</li>
             <li>Precipitation: </li>
             <li>Humidity: </li>
             <li>Wind: </li>
           </ul>
         </div>
+        <div className="col-7 forecast">blaaaa</div>
       </div>
     </div>
   );
